a3608817ad0fc733253f2c9ae364ac9541892e66
Art Navsegda <artnavsegda@gmail.com>
2025-07-23 10:44:22 +0300

    raylib

--- src/hello.c
+++ src/hello.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(800, 450, "hello window");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Hello !", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
